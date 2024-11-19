7409075e46eb12d49aaf3ba45692c64d8f4362b5
LeaCavalli <lea.cavalli01@gmail.com>
2024-11-19 12:15:00 +0100

    modif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 
 @Component({
@@ -16,6 +17,7 @@ import { FooterComponent } from './footer/footer.component';
             MatButtonModule,
             HeaderComponent,
             FooterComponent,
+            AccueilComponent,
           ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
